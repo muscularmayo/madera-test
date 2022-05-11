dd12c3b21404b7c73354638caaf1f5d57d4d20ba
Chris Risden <christopherrisden@gmail.com>
2022-05-11 15:40:34 -0700

    added some rows and stuff

--- app.js
+++ app.js
@@ -15,7 +15,43 @@ let originalData
 
 (async function() {
   const promiseData = await fsp.readFile('./dbadeveloperremoteexamination/StudentData.csv', 'utf8')
-  console.log(promiseData)
+  const dataArray = promiseData.split('\r\n')
+  /*
+  dataArray[0] is going to be our object headers
+  dataArray[1] to [infinity] are going to be our objects
+  we want to convert this into a array of objects
+  */
+  const headers = dataArray[0].split(',')
+  const studentObjects = []
+  for (let i = 1; i < dataArray.length; i++) {
+    const currentStudent = dataArray[i].split(',')
+    const studentObject = {}
+    for (let j = 0; j < headers.length; j++) {
+      studentObject[headers[j]] = currentStudent[j]
+    }
+    studentObjects.push(studentObject)
+  }
+
+  for(let i = 0; i < studentObjects.length; i++) {
+    //we are now in the object
+    let majorityElective;
+    if (studentObjects[i].NumberOfElectives/studentObjects[i].NumberOfClasses >= .5) {
+      majorityElective = true;
+    } else {
+      majorityElective = false;
+    }
+    studentObjects[i].hasMajorityElective = majorityElective;
+
+    //“isELandFRE” true if 'isEnglishLearner' and 'isFreeAndReduced'
+    if (studentObjects[i].isEnglishLearner === '1' && studentObjects[i].isFreeAndReduced === '1') {
+      studentObjects[i].isELandFRE = true;
+    } else {
+      studentObjects[i].isELandFRE = false;
+    }
+
+  }
+  console.log(studentObjects)
+
 })();
 
 
